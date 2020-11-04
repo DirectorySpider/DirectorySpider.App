074cc6b1a0ccfecdd79a200d55ac4461c91a0539
Jirubizu <37895773+Jirubizu@users.noreply.github.com>
2020-11-04 06:35:18 +0000

    init

---
+++ CrawlUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryCrawler
+{
+    internal static class CrawlUtils
+    {
+        public static void SearchDirectory(DirectoryInfo dir)
+        {
+            FileInfo[] files = null;
+
+            try
+            {
+                files = dir.GetFiles();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            if (files != null)
+            {
+                Program.jsonDir.AppendLine($"{'{'}\"{dir.Name}\": [");
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Program.jsonDir.AppendLine($"\"{files[i].Name}\"{((i == files.Length - 1 && dir.GetDirectories().Length == 0) ? ' ' : ',')}");
+                }
+
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    SearchDirectory(dirs[i]);
+                    if (dirs.Length > 1 && i != dirs.Length-1)
+                    {
+                        Program.jsonDir.AppendLine(",");
+                    }
+                }
+
+                Program.jsonDir.AppendLine("]}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DirectoryCrawler
+{
+    class Program
+    {
+        public static StringBuilder jsonDir = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo("./");
+            StreamWriter file = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"/{dir.Name}.json", true);
+            CrawlUtils.SearchDirectory(dir);
+            JToken token = JToken.Parse(jsonDir.ToString());
+            file.WriteLine(token.ToString(Formatting.Indented));
+            file.Close();
+        }
+    }
+}
